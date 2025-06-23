81ad17d99e3589bc0b13a8430bc4fe4176872b31
SteNuf <stephan.nufer@gmx.de>
2025-06-23 21:20:40 +0200

    this commit add the trial for the DOM.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const objectNotice = [
+let objectNotice = [
   {
     title: "Notiz 1",
     content: "Die ist ein kleiner Text.",
@@ -20,34 +20,36 @@ const objectNotice = [
     lastUpdate: "",
   },
 ];
-
 console.log(objectNotice);
 
-const noticeEntryListDiv = document.createElement("div");
-noticeEntryListDiv.classList.add("notice-entry-list");
-noticeEntryListDiv.classList.add("select-actuality");
-noticeEntryListDiv.setAttribute("data-id", "6");
 
-const noticeEntryListTitleDiv = document.createElement("div");
-noticeEntryListTitleDiv.classList.add("notice-entry-list-title");
-const noticeEntryListTitle = document.createTextNode("Test 1");
+  
+  const noticeEntryListDiv = document.createElement("div");
+  noticeEntryListDiv.classList.add("notice-entry-list");
+  noticeEntryListDiv.classList.add("select-actuality");
+  noticeEntryListDiv.setAttribute("data-id", "6");
 
-const noticeEntryListContentDiv = document.createElement("div");
-noticeEntryListContentDiv.classList.add("notice-entry-list-content");
+  const noticeEntryListTitleDiv = document.createElement("div");
+  noticeEntryListTitleDiv.classList.add("notice-entry-list-title");
+  const noticeEntryListTitle = document.createTextNode(test.title);
 
-const noticeEntryListContent = document.createTextNode("sdhdsjhdsjdsl");
+  const noticeEntryListContentDiv = document.createElement("div");
+  noticeEntryListContentDiv.classList.add("notice-entry-list-content");
 
-const noticeEntryListDateDiv = document.createElement("div");
-noticeEntryListDateDiv.classList.add("notice-entry-list-date");
+  const noticeEntryListContent = document.createTextNode("sdhdsjhdsjdsl");
 
-const noticeEntryListDate = document.createTextNode("10.06.2025");
+  const noticeEntryListDateDiv = document.createElement("div");
+  noticeEntryListDateDiv.classList.add("notice-entry-list-date");
 
-noticeEntryListTitleDiv.appendChild(noticeEntryListTitle);
-noticeEntryListContentDiv.appendChild(noticeEntryListContent);
-noticeEntryListDateDiv.appendChild(noticeEntryListDate);
+  const noticeEntryListDate = document.createTextNode("10.06.2025");
 
-noticeEntryListDiv.appendChild(noticeEntryListTitleDiv);
-noticeEntryListDiv.appendChild(noticeEntryListContentDiv);
-noticeEntryListDiv.appendChild(noticeEntryListDateDiv);
+  noticeEntryListTitleDiv.appendChild(noticeEntryListTitle);
+  noticeEntryListContentDiv.appendChild(noticeEntryListContent);
+  noticeEntryListDateDiv.appendChild(noticeEntryListDate);
 
-console.log(noticeEntryListDiv);
+  noticeEntryListDiv.appendChild(noticeEntryListTitleDiv);
+  noticeEntryListDiv.appendChild(noticeEntryListContentDiv);
+  noticeEntryListDiv.appendChild(noticeEntryListDateDiv);
+
+  document.getElementById("notes-lists").appendChild(noticeEntryListDiv);
+
