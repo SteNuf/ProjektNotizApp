814d0765c18f0f7994b02372f0e2338adc46620d
SteNuf <stephan.nufer@gmx.de>
2025-07-04 14:40:19 +0200

    This commit add the new API file.

--- index.js
+++ index.js
@@ -1,52 +1,6 @@
 const notesListElement = document.querySelector('.notes-list');
 
 
-const MOCK_Notes =
-[
-  {
-    title: "Notiz 1",
-    content: "Die ist ein kleiner Text.",
-    id: 1,
-    lastUpdate: 1751626816836,
-  },
-
-  {
-    title: "Notiz 2",
-    content: "Lorem ipsum zur Notiz 2.",
-    id: 2,
-    lastUpdate: 1751626826836,
-  },
-
-  {
-    title: "Notiz 3",
-    content: "Lorem Ipsum",
-    id: 3,
-    lastUpdate: 1751626836836,
-  },
-
-  {
-    title: "Notiz 4",
-    content: "Lorem Ipsum",
-    id: 4,
-    lastUpdate: 1751626846836,
-  },
-
-
-  {
-    title: "Notiz 5",
-    content: "Lorem Ipsum",
-    id: 5,
-    lastUpdate: 1751626856836,
-  },
-
-  {
-    title: "Notiz 6",
-    content: "Lorem Ipsum",
-    id: 6,
-    lastUpdate:1751626866836,
-  },
-];
-
 function displayNotesList() {
   const notes = MOCK_Notes;
 
